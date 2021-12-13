db092ca8850ccde89e3e3e9581f8c2341285dc85
SaadNiaziDev <saad.niaxi47@gmail.com>
2021-12-13 14:11:15 +0500

    basic structure

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const multer = require('multer');
+
+const app = express();
+
+app.use(port(3000));
+
+app.listen(()=>{
+Console.log('listening on port 3000 ');
+});
